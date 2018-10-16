102511a347c4b5e48df782d51e6310faf7d2f974
Josh McCall <joshmccall221@gmail.com>
2018-10-15 18:15:41 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -29,8 +29,8 @@
  
         autoPlay: 5000, //Set AutoPlay to 5 seconds
 
-        items : 4,
-        itemsDesktopSmall : [979,3],
+        items : 2,
+        itemsDesktopSmall : [979,2],
         stopOnHover: true
  
     });
